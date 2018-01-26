2a7fb578017d1c3a481f84f53d623201dc57e1fe
root <root@localhost.localdomain>
2018-01-26 11:23:34 +0800

    add check_http_code函数

--- check_http_log.sh
+++ check_http_log.sh
@@ -15,7 +15,7 @@ log_path=/var/log/apache2/access.log
 
 #自定义函数，用数组表示状态码
 check_http_status() {
-	http_status_code=($(cat $log_path |awk '{print $7}'|awk '{ 
+	http_status_codes=($(cat $log_path |awk '{print $7}'|awk '{ 
 					if ($1 <100) { a++ }
 					else if ($1>=100 && $1<200) { b++ }
 					else if ($1>=100 && $1<300) { c++ }
@@ -26,14 +26,53 @@ check_http_status() {
 						print a?a:0,b?b:0,c?c:0,d?d:0,e?e:0,f?f:0,a+b+c+d+e+f 
 						}'))
 
-echo -e '\E[33m' "The number of http status code[100-]:" ${resetsem} ${http_status_code[0]}
-echo -e '\E[33m' "The number of http status code[100+]:" ${resetsem} ${http_status_code[1]}
-echo -e '\E[33m' "The number of http status code[200+]:" ${resetsem} ${http_status_code[2]}
-echo -e '\E[33m' "The number of http status code[300+]:" ${resetsem} ${http_status_code[3]}
-echo -e '\E[33m' "The number of http status code[400+]:" ${resetsem} ${http_status_code[4]}
-echo -e '\E[33m' "The number of http status code[500+]:" ${resetsem} ${http_status_code[5]}
-echo -e '\E[33m' "All the request:" ${resetsem} ${http_status_code[6]}
+echo -e '\E[33m' "The number of http status codes[100-]:" ${resetsem} ${http_status_codes[0]}
+echo -e '\E[33m' "The number of http status codes[100+]:" ${resetsem} ${http_status_codes[1]}
+echo -e '\E[33m' "The number of http status codes[200+]:" ${resetsem} ${http_status_codes[2]}
+echo -e '\E[33m' "The number of http status codes[300+]:" ${resetsem} ${http_status_codes[3]}
+echo -e '\E[33m' "The number of http status codes[400+]:" ${resetsem} ${http_status_codes[4]}
+echo -e '\E[33m' "The number of http status codes[500+]:" ${resetsem} ${http_status_codes[5]}
+echo -e '\E[33m' "All the request:" ${resetsem} ${http_status_codes[6]}
 }
 
 check_http_status
+
+#输入具体状态码数据
+
+check_http_code() {
+	http_code=($(cat $log_path |awk '{print $7}'|awk '{ 
+							if ($1 != "")
+								{ code[$1]++;total++}
+							else
+								{exit}
+							} END{
+								print code[403]?code[403]:0,code[404]?code[404]:0,total
+							}'))
+
+echo -e '\E[33m' "The number of http status[403]:" ${resetsem} ${http_code[0]}
+echo -e '\E[33m' "The number of http status[403]:" ${resetsem} ${http_code[1]}
+echo -e '\E[33m' "ALL the request:" ${resetsem} ${http_code[2]}
+
+					
+
+}
+
+check_http_code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
