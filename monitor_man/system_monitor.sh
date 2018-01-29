1f49008fd7a0d5d096eb70b75c24612929693720
duben5945 <542405296@qq.com>
2018-01-29 11:52:29 +0800

    complete system_monitor.sh

--- monitor_man/system_monitor.sh
+++ monitor_man/system_monitor.sh
@@ -25,6 +25,7 @@
 #System Uptime
 #echo -e '\E[1;32m'   ${reset_terminal}
 ##################################################################
+clear
 reset_terminal=$(tput sgr0)
 #网络连接情况
 echo -ne '\E[1;32m' "Internet: "  ${reset_terminal}
@@ -52,4 +53,26 @@ echo -e '\E[1;32m'  "Externet IP: " ${reset_terminal} ${external_ip}
 #DNS服务器
 server_name=$(cat /etc/resolv.conf|awk '/nameserver/ {print $2}')
 echo -e '\E[1;32m'  "Server Name: " ${reset_terminal} ${server_name}
-#
+#登录账号
+who >/tmp/who
+echo -e '\E[1;32m'  "Logged In Users: " ${reset_terminal} 
+cat /tmp/who
+#内存使用
+free -h >/tmp/ramcache
+echo -e '\E[1;32m'  "Ram Usages: " ${reset_terminal} 
+cat /tmp/ramcache|grep -iv "swap"
+#交换分区使用
+echo -e '\E[1;32m'  "Swap Usages: " ${reset_terminal} 
+cat /tmp/ramcache|grep -iv "mem"
+#硬盘使用
+df -Ph |grep -iv "tmpfs" |awk '{print $1 "\t" $5}'>>/tmp/diskusages
+echo -e '\E[1;32m'  "Disk Usages: " ${reset_terminal} 
+cat /tmp/diskusages
+#系统CPU使用率
+cpu_usages=$(uptime |awk '{print $8 $9 $10}')
+echo -e '\E[1;32m'  "Load Average: " ${reset_terminal} ${cpu_usages}
+#系统开机时间
+cputime=$(uptime |awk '{print $3}'|tr -d "," )
+echo -e '\E[1;32m'  "System Uptime: " ${reset_terminal} ${cputime}
+
+rm  /tmp/who /tmp/ramcache /tmp/diskusages
